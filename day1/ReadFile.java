ec261a1917c4ba1d3752d4053ed70fa7f94a76de
Leo Laverty <leolav95@outlook.com>
2021-12-03 14:31:19 +0100

    Day 1 started.
    Function added to parse text file.
    Second function added to convert text to array.
    Skeleton added for comparison of values in the array.

--- day1/ReadFile.java
+++ day1/ReadFile.java
@@ -2,6 +2,7 @@ package day1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ReadFile {
@@ -18,5 +19,21 @@ public class ReadFile {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+    }
+
+    public static int compareValues(Scanner reader){
+        ArrayList<Integer> values = new ArrayList<Integer>();
+        int count = 0;
+        while (reader.hasNextLine()){
+            String data = reader.nextLine();
+            values.add(Integer.parseInt(data));
+        }
+        for(int i=1; i < values.size(); i++){
+            
+            if(values.get(i) > values.get(i-1)){
+
+            }
+        }
+        return count;
     }
 }
