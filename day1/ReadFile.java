249ece457ece5878bdb0d80d3fa046660af84064
Leo Laverty <leolav95@outlook.com>
2021-12-03 21:31:13 +0100

    Day 1 challenge complete

--- day1/ReadFile.java
+++ day1/ReadFile.java
@@ -6,34 +6,38 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ReadFile {
+    int count = 0;
     public static void main(String[] args) {
         try {
-            File myObj = new File("input.txt");
+            File myObj = new File("./input.txt");
             Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
-            }
-            myReader.close();
+            ReadFile app = new ReadFile();
+            app.compareValues(myReader);
+            app.printCount();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
     }
 
-    public static int compareValues(Scanner reader){
-        ArrayList<Integer> values = new ArrayList<Integer>();
-        int count = 0;
+    public int compareValues(Scanner reader){
+        ArrayList<Integer> values = new ArrayList<>();
         while (reader.hasNextLine()){
             String data = reader.nextLine();
             values.add(Integer.parseInt(data));
         }
+        reader.close();
         for(int i=1; i < values.size(); i++){
-            
             if(values.get(i) > values.get(i-1)){
-
+                count++;
+                System.out.println(values.get(i) + " (Value Increased)");
+            }else{
+                System.out.println(values.get(i) + " (Value Decreased)");
             }
         }
         return count;
+    }
+    public void printCount(){
+        System.out.println("Total increased count: " + count);
     }
 }
